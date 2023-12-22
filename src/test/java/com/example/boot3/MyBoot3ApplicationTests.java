c31eaaff42abc3de2e43a1b59381b8218529b319
MENGJIAO <mengjiao3021@gmail.com>
2023-12-23 03:36:39 +0800

    chore: 工程初始化

---
+++ src/test/java/com/example/boot3/MyBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBoot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
