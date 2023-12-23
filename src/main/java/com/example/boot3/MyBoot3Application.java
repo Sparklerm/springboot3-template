607da4aac0b0398ad8fa88f81de9dc29ca15a91e
MENGJIAO <mengjiao3021@gmail.com>
2023-12-23 21:58:35 +0800

    chore: Spring Boot 数据源配置 1. MySQL hikari 配置 2. Redis，Redisson配置 3. MyBatisPlus 配置

--- src/main/java/com/example/boot3/MyBoot3Application.java
+++ src/main/java/com/example/boot3/MyBoot3Application.java
@@ -3,6 +3,9 @@ package com.example.boot3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 项目启动类
+ */
 @SpringBootApplication
 public class MyBoot3Application {
 
